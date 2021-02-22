82dd94697fcc84306845c4313276e088bdbbfaf6
Luandrada <luandrada12@live.com>
2021-02-22 13:35:36 -0300

    mobile

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal.component.scss']
 })
 export class PrincipalComponent implements OnInit {
-
+ menuIsOpen:boolean=false
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openMenu(){
+    let menu= document.getElementById("options");
+    let close = document.getElementById("close");
+    let menuIcon= document.getElementById("menu");
+    if (this.menuIsOpen) {
+      menu.style.display="none";
+      close.style.display="none";
+      menuIcon.style.display="block";
+      menuIcon.style.visibility="unset";
+      this.menuIsOpen=false;
+    }else{
+      menu.style.display="flex";
+      close.style.display="block";
+      menuIcon.style.display="none";
+      menuIcon.style.visibility="hidden";
+      this.menuIsOpen=true;
+    }
+
+  }
+  closeMenu(){
+
+  }
 }
